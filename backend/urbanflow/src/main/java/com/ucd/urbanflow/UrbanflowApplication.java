842689630b16bd0169890ddcd031f938980b14c3
xinnan696 <xinnan.mu@ucdconnect.ie>
2025-06-16 18:24:13 +0100

    commit backend requirement

---
+++ backend/urbanflow/src/main/java/com/ucd/urbanflow/UrbanflowApplication.java
@@ -0,0 +1,13 @@
+package com.ucd.urbanflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanflowApplication.class, args);
+	}
+
+}
