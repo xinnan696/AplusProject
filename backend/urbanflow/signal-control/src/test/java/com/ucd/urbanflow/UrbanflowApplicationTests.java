7d1f69e65880858016417f9a8dccb69a9028b913
xinnan696 <xinnan.mu@ucdconnect.ie>
2025-06-18 15:52:44 +0100

    modify configuration

---
+++ backend/urbanflow/signal-control/src/test/java/com/ucd/urbanflow/UrbanflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucd.urbanflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/urbanflow/traffic-sensing/src/main/java/com/ucd/urbanflow/TrafficSensingApplication.java
@@ -0,0 +1,13 @@
+package com.ucd.urbanflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficSensingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficSensingApplication.class, args);
+	}
+
+}
