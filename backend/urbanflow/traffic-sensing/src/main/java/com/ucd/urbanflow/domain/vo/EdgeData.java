96930fc1f0824b24ce49331ba09fc449f551c7bb
xinnan696 <xinnan.mu@ucdconnect.ie>
2025-06-23 00:24:56 +0100

    Feature/traffic sensing mxn (#8)
    * commit backend requirement
    
    * update project configuration
    
    * modify configuration
    
    * modify configuration
    
    * modify configuration
    
    * query congested road data

---
+++ backend/urbanflow/traffic-sensing/src/main/java/com/ucd/urbanflow/domain/vo/EdgeData.java
@@ -0,0 +1,17 @@
+package com.ucd.urbanflow.domain.vo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EdgeData {
+
+    @JsonProperty("edgeID")
+    private String edgeID;
+
+    @JsonProperty("vehicleCount")
+    private int vehicleCount;
+
+}
