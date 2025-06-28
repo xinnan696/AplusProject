39131cf43584a92d1ebc40d227f41c56e0497bbb
zijian-111 <zijianxu111@gmail.com>
2025-06-28 14:32:00 +0100

    fix: replace urbanflow_frontend with correct structure

--- frontend/urbanflow_frontend/src/router/index.ts
+++ frontend/urbanflow_frontend/src/router/index.ts
@@ -2,12 +2,24 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import ControlHome from '@/views/control/ControlHome.vue'
 import MapPage from '@/views/Map/MapPage.vue'
 import LoginLogin from '@/views/login/LoginLogin.vue'
+import LoginReset from '@/views/login/LoginReset.vue'
+import LoginForget from '@/views/login/LoginForget.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/login',
     name: 'LoginLogin',
     component: LoginLogin
+  },
+  {
+    path: '/forget',
+    name: 'LoginForget',
+    component: LoginForget
+  },
+  {
+    path: '/reset',
+    name: 'LoginReset',
+    component: LoginReset
   },
   {
     path: '/',
